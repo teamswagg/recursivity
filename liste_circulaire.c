71d1dfe68b78df321b083a661d9015ad3b5c2394
Captain-Chan <davy_cd@hotmail.com>
2014-03-21 16:13:48 +0100

    davy

---
+++ liste_circulaire.c
@@ -0,0 +1,30 @@
+#include "josephusList.h"
+
+#include <assert.h>
+#include <stdlib.h>
+
+struct jCircle
+{
+    int soldierNumber;
+
+    Soldier* pFirstS;
+    Soldier* pCurrentS;
+};
+
+typedef struct node Soldier;
+
+JCircle* pJCircle(int soldierNumber)
+{
+    assert(soldierNumber > 0);
+
+    JCircle* pJCircle;
+    if(!(pJCircle = malloc(sizeof(JCircle))))
+        return NULL;
+
+    pJCircle->soldierNumber = soldierNumber;
+    pJCircle->pFirstS = NULL;
+    pJCircle->pCurrentS = NULL;
+
+    return pJCircle;
+}
+
---
+++ liste_circulaire.h
@@ -0,0 +1,10 @@
+#ifndef JOSEPHUS_LIST_H
+#define JOSEPHUS_LIST_H
+
+typedef struct jCircle JCircle;
+typedef struct node Soldier;
+
+JCircle* pJCircle(int soldierNumber);
+
+#endif
+
